91e4f55cad8364934e88b31471b8c78a70904adb
wokeoke <woke.oke@gmail.com>
2022-04-30 21:00:23 +0700

    Error Checking - Controller Example

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,14 @@
 const { StatusCodes } = require('http-status-codes');
 const User = require('../models/User');
+const { BadRequestError } = require('../errors');
 
 const register = async (req, res) => {
-  const user = await User.create({ ...req.body });
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    throw new BadRequestError('Please provide name, email, and password');
+  }
 
+  const user = await User.create({ ...req.body });
   res.status(StatusCodes.CREATED).json({ user });
 };
 
