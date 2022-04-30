39428fa061e2d0765ce5f9f269a945aae986c08d
wokeoke <woke.oke@gmail.com>
2022-04-30 20:55:17 +0700

    Create User - Basic Setup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,10 @@
+const { StatusCodes } = require('http-status-codes');
+const User = require('../models/User');
+
 const register = async (req, res) => {
-  res.send('Register User');
+  const user = await User.create({ ...req.body });
+
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
