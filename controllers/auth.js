d68a815188176b3880a82189e25146ba86e47579
wokeoke <woke.oke@gmail.com>
2022-04-30 21:28:05 +0700

    Generate Token - Instance Method

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,11 +4,7 @@ const User = require('../models/User');
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
-
-  const token = jwt.sign({ userId: user._id, name: user.name }, 'jwtsecret', {
-    expiresIn: '30d',
-  });
-
+  const token = user.createJWT();
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
